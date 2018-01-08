73b6b23ea575316e470ca561471606e442ba25bb
Treinaweb <treinaweb@Treinaweb.local>
2018-01-08 15:02:48 -0200

    10 - rotas nomeadas

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,22 @@ Route::get('/', function () {
 
 Route::get('clients', function () {
     return '<h1>Lista de Cliente</h1>';
-});
+})->name('clientes');
 
 Route::get('clients/create/new', function () {
-    return '<h1>Criar Cliente</h1>';
+    $html = '<h1>Criar Cliente</h1>';
+
+    $html .= '<br><a href="' . route('clientes') . '">Lista de clientes</a>';
+
+    return $html;
+
 });
 
 Route::get('clients/{name}/{age?}', function ($name, $age='nao definido') {
-    return "Detalhes do cliente {$name} ele tem {$age} anos";
+    $html = "Detalhes do cliente {$name} ele tem {$age} anos";
+    
+    $html .= '<br><a href="' . route('clientes') . '">Lista de clientes</a>';
+    
+    return $html;
+
 })->where(['age' => '[0-9]+', 'name' => '[A-Za-z]+']);

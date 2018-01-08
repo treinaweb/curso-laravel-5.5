483b04ddb47d7c9bda5ad28363f1e47bc158bf40
Treinaweb <treinaweb@Treinaweb.local>
2018-01-08 15:17:05 -0200

    12 - outros verbos http

--- routes/web.php
+++ routes/web.php
@@ -24,18 +24,32 @@ Route::prefix('treinaweb/clients')->group(function () {
     Route::get('create/new', function () {
         $html = '<h1>Criar Cliente</h1>';
 
+        $html .= '<br>
+                    <form action="' . route('clients.save') . '" method="POST">
+                        <input type="hidden" name="_token" value="' . csrf_token() . '">
+                        <input type="hidden" name="_method" value="PUT">
+                        Nome: <input type="text" name="name"><br>
+                        Idade: <input type="text" name="age"><br>
+                        <input type="submit" value="Salvar">
+                    </form>
+                    ';
+
         $html .= '<br><a href="' . route('clients.list') . '">Lista de clientes</a>';
 
         return $html;
 
     });
 
-    Route::get('{name}/{age?}', function ($name, $age='nao definido') {
-        $html = "Detalhes do cliente {$name} ele tem {$age} anos";
-        
-        $html .= '<br><a href="' . route('clients.list') . '">Lista de clientes</a>';
-        
-        return $html;
+    Route::put('save', function () {
+        return 'Cliente criado com sucesso';
+    })->name('clients.save');
 
-    })->where(['age' => '[0-9]+', 'name' => '[A-Za-z]+']);
+    // Route::get('{name}/{age?}', function ($name, $age='nao definido') {
+    //     $html = "Detalhes do cliente {$name} ele tem {$age} anos";
+        
+    //     $html .= '<br><a href="' . route('clients.list') . '">Lista de clientes</a>';
+        
+    //     return $html;
+
+    // })->where(['age' => '[0-9]+', 'name' => '[A-Za-z]+']);
 });

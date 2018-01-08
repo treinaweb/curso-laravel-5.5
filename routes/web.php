7ec45ccca5c93d0f3c0aa3474f6fd2ff05eab70b
Treinaweb <treinaweb@Treinaweb.local>
2018-01-08 15:21:56 -0200

    14 - rota de redirecionamento

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::redirect('cliente', 'treinaweb/clients', 301);
+
 Route::prefix('treinaweb/clients')->group(function () {
     
     Route::get('/', function () {

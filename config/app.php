1847d8b135d8e281ed387967174d1319fc912f48
Treinaweb <treinaweb@Treinaweb.local>
2018-01-30 11:53:49 -0200

    64 - instalando package

--- config/app.php
+++ config/app.php
@@ -178,6 +178,12 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\ValidationServiceProvider::class,
 
+
+        /**
+         * DOMPDF
+         */
+        Barryvdh\DomPDF\ServiceProvider::class,
+
     ],
 
     /*
@@ -226,6 +232,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 

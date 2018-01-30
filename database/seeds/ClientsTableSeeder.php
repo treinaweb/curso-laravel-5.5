56c8fb24ab0152c113298e6b83e27a308bd0e677
Treinaweb <treinaweb@Treinaweb.local>
2018-01-30 15:14:17 -0200

    67 - seeders

---
+++ database/seeds/ClientsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Client;
+use Illuminate\Database\Seeder;
+
+class ClientsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Client::insert([
+            'name'      => str_random(10),
+            'email'     => str_random(11) . "@" . str_random(7) . '.' . str_random(3),
+            'age'       => rand(0, 100),
+            'photo'     => str_random(100),
+            'user_id'   => 1
+        ]);
+    }
+}

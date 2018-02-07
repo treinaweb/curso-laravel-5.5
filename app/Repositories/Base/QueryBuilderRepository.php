22d8fa3cf9fb5617e93865efd66d80618381352f
Treinaweb <treinaweb@Treinaweb.local>
2018-02-07 16:18:00 -0200

    79 - repositorio base

---
+++ app/Repositories/Base/QueryBuilderRepository.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Repositories\Base;
+
+use DB;
+
+abstract class QueryBuilderRepository implements RepositoryInterface
+{
+
+  /**
+   * Nome da tabela
+   *
+   * @var String
+   */
+  protected $table;
+
+  /**
+   * Retorna lista paginada
+   *
+   * @param integer $page
+   * @return void
+   */
+  public function getAll(int $page = 10)
+  {
+    return DB::table($this->table)->paginete($page);
+  }
+
+  /**
+   * Retorna registro por id
+   *
+   * @param integer $id
+   * @return void
+   */
+  public function find(int $id)
+  {
+    return DB::table($this->table)->where('id', $id)->first();
+  }
+
+  /**
+   * Cria um registro pelo array de dados
+   *
+   * @param array $data
+   * @return void
+   */
+  public function create(array $data)
+  {
+    return DB::table($this->table)->insert($data);
+  }
+
+  /**
+   * Atualiza um registro no banco
+   *
+   * @param integer $id
+   * @param array $data
+   * @return void
+   */
+  public function update(int $id, array $data)
+  {
+    return DB::table($this->table)->where('id', $id)->update($data);
+  }
+
+  /**
+   * Apaga um registro no banco
+   *
+   * @param integer $id
+   * @return void
+   */
+  public function delete(int $id)
+  {
+    return DB::table($this->table)->where('id', $id)->delete();
+  }
+
+
+}
---
+++ app/Repositories/Base/RepositoryInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repositories\Base;
+
+interface RepositoryInterface 
+{
+  public function getAll(int $page);
+  public function find(int $id);
+  public function create(array $data);
+  public function update(int $id, array $data);
+  public function delete(int $id);
+}
